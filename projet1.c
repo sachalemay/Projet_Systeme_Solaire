ea6cd13ec4fa9200a10b9ebe6e4bd75ee48e14bd
antoinegvincent <antoine.vincet@epfl.ch>
2020-10-19 20:11:02 +0200

    Ajout de la structure pour les donnees des planetes

--- projet1.c
+++ projet1.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+struct donneePlanet {
+	double planetName;
+	double demiGrandAxe; 		//ou Aphelie
+	double demiPetitAxe; 		//ou Perihelie
+	double eccentricity; 		//pour calcul de l'elipse
+	double masse; 				//pour force de gravitation
+	double radius;				//pour etablir la collision
+	double fullOrbitTime;		//pour vitesse planete
+	double gravity 				//si on est chaud
+	double rotationTime			//si on est chaud
+};
+
+//Il va peut-être falloir s'interesser a Kepler pour la vitesse de rotation autour du soleil
+
 int main(int argc, char * argv[]) {
 	
 } 
