4b9f44e7a41e19aba87bfed8bb6c9dd0c83469e4
antoinegvincent <antoine.vincet@epfl.ch>
2020-10-21 16:59:59 +0200

    Structure Meteorite et force pour calcul de gravitation

--- projet1.c
+++ projet1.c
@@ -2,18 +2,37 @@
 #include <math.h>
 
 struct donneePlanet {
-	char * planetName;
-	double demiGrandAxe; 		//ou Aphelie
-	double demiPetitAxe; 		//ou Perihelie
-	double eccentricity; 		//pour calcul de l'elipse
-	double masse; 					//pour force de gravitation
-	double radius;					//pour etablir la collision
-	double fullOrbitTime;		//pour vitesse planete
+	char * planetName;			//"name"
+	double demiGrandAxe; 		//ou "aphelion"
+	double demiPetitAxe; 		//ou "perihelion"
+	double masse; 					// "massValue" + "massExponent"
+	double radius;					//"equaRadius"
+	double fullOrbitTime;		//"sideralOrbit"
 	double gravity; 				//si on est chaud
 	double rotationTime;			//si on est chaud
 	double distanceSoleil;
 	double x_cartesien;
 	double y_cartesien;
+};
+
+struct meteorite {
+	double initialspeed_x;
+	double initialspeed_y;
+	double distanceSoleil;
+	double distancePlanet;
+	double masse;
+	double radius;
+	double x_cartesien;
+	double y_cartesien;
+};
+
+struct forceCaract{
+	double intensitee;
+	double angle;
+	double direction_parrallele_sun;
+	double direction_perpendiculaire_sun;
+	double direction_x;
+	double direction_y;
 };
 
 void avancementParametrisation ( int time, struct donneePlanet * planet){
@@ -22,13 +41,20 @@ void avancementParametrisation ( int time, struct donneePlanet * planet){
 	planet->y_cartesien=planet->demiPetitAxe*sin(time*2*M_PI/planet->fullOrbitTime);
 }
 
-
+void gravitationalForce (double gravitationalConstant, struct donneePlanet * planet, struct meteorite * meteor, struct forceCaract * force){
+	meteor->distancePlanet = sqrt(pow(planet->x_cartesien-meteor->x_cartesien,2)+pow(planet->y_cartesien-meteor->y_cartesien,2));
+	double intensitee = gravitationalForce * planet->masse * meteor->masse / pow(distancePlanet,2);
+	double angle = acos( pow(distancePlanet,2) + pow (meteor->distanceSoleil,2) - pow(planet->distanceSoleil,2) / (2* distancePlanet * meteor->distanceSoleil);
+	force->direction_parrallele_sun += intensitee*cos(angle);
+	force->direction_perpendiculaire_sun += intensitee*sin(angle);
+}
 
 //Il va peut-être falloir s'interesser a Kepler pour la vitesse de rotation autour du soleil
 
 int main(int argc, char * argv[]) {
 	
-	int time= 481;
+	int time= 2112;
+	double gravitationalForce = 6.6743*pow(10,-17);
 	
 	struct donneePlanet mercure;
 	mercure.planetName= "Mercure";
