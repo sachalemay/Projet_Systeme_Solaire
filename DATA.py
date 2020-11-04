ca0fd8bb2e26d5dbe7ae299df47bbd46b284b246
Sacha Le May <sacha.lemay@epfl.ch>
2020-11-04 22:07:58 +0100

    création du fichier csv bodies.csv

--- DATA.py
+++ DATA.py
@@ -7,29 +7,43 @@ Created on Wed Oct 21 17:02:56 2020
 """
 
 import json 
+import csv 
+   
+list = ["list_name","list_aphelion","list_perihelion","list_semimajorAxis", "list_mass","list_equaRadius", "list_sideralOrbit", "list_gravity"]
+list_labels = ["name","aphelion","perihelion","semimajorAxis", "mass","equaRadius", "sideralOrbit", "gravity"]
 
-Solar_Syst_list = ["jupiter", "soleil", "mars", "pluton"]
-
-final = []
-
-for name in Solar_Syst_list: 
-    with open ('{%s}.json' % name) as f: 
-        celestarobj = json.load(f)
+with open ('bodies.json') as f:       
+    celestarobj = json.load(f)
+    
+with open ('bodies.csv', "a", newline = "") as f:  
+    
+    writer_csv = csv.writer(f)    
+    writer_csv.writerow(list_labels)
+    for body in celestarobj["bodies"]: 
+        mass_dict = body['mass']
+        body['mass'] = float(mass_dict['massValue'])*10**(float(mass_dict['massExponent'])) 
         
-        mass_dict = celestarobj['mass']
-        celestarobj['mass'] = float(mass_dict['massValue'])*10**(float(mass_dict['massExponent']))
-
-        celestarobj.pop('englishName')
-        celestarobj.pop('eccentricity') #eccentricté utile? 
-        celestarobj.pop('vol')
-        celestarobj.pop('density')
-        celestarobj.pop('escape')
-        celestarobj.pop('flattening')
-        celestarobj.pop('dimension')
-        celestarobj.pop('discoveredBy')
-        celestarobj.pop('discoveryDate')
-        celestarobj.pop('polarRadius')
-        celestarobj.pop('alternativeName')
-        celestarobj.pop('moons')  #l'affichage des lunes 
-        final.append(celestarobj)
-
+        body.pop('id')
+        body.pop('aroundPlanet')
+        body.pop('englishName')
+        body.pop('eccentricity') #eccentricté utile? 
+        body.pop('vol')
+        body.pop('density')
+        body.pop('escape')
+        body.pop('flattening')
+        body.pop('dimension')
+        body.pop('discoveredBy')
+        body.pop('discoveryDate')
+        body.pop('polarRadius')
+        body.pop('alternativeName')
+        body.pop('moons')  #l'affichage des lunes 
+        body.pop('rel')      
+        body.pop('inclination') 
+        body.pop('axialTilt')
+        body.pop('sideralRotation')
+    
+    
+        if body["isPlanet"] == True: 
+            list_body_data = [body[label] for label in list_labels]
+            writer_csv.writerow(list_body_data)
+        
