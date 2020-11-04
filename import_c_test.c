ce8c4d6e09e9be765e2afedac96d9bf056723787
Sacha Le May <sacha.lemay@epfl.ch>
2020-11-04 22:12:01 +0100

    Lecture du fichier CSV de données (sur fichier de test c). Pb de nom pour chaque planete importée dans la structure (à régler)

---
+++ import_c_test.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct donneePlanet {
+	char * planetName;			
+	double aphelion;
+	double perihelion; 	
+	double demiGrandAxe; 		
+	double masse; 					
+	double radius;					
+	double fullOrbitTime;
+	double gravity; 				
+	double rotationTime;			
+	double distanceSoleil;
+	double x_cartesien;
+	double y_cartesien;
+	double distanceAsteroide;
+};
+
+void afficher(struct donneePlanet * planete) {
+    printf("planetName : %s aphelion : %f masse: %f\n", planete->planetName, planete->aphelion, planete->masse);
+}
+
+
+int lireLigne(char * ligne, struct donneePlanet * planet) {
+	char * virgule1 = strchr(ligne, ',');
+	if (virgule1 == NULL) return 0;
+	char * virgule2 = strchr(virgule1 + 1, ',');
+	if (virgule2 == NULL) return 0;
+	char * virgule3 = strchr(virgule2 + 1, ',');
+	if (virgule3 == NULL) return 0;
+	char * virgule4 = strchr(virgule3 + 1, ',');
+	if (virgule4 == NULL) return 0;
+	char * virgule5 = strchr(virgule4 + 1, ',');
+	if (virgule5 == NULL) return 0;
+	char * virgule6 = strchr(virgule5 + 1, ',');
+	if (virgule6 == NULL) return 0;
+	char * virgule7 = strchr(virgule6 + 1, ',');
+	if (virgule7 == NULL) return 0;
+		
+
+	planet->aphelion = atof(virgule1 + 1);
+	planet->perihelion = atof(virgule2 + 1);
+	planet->demiGrandAxe = atof(virgule3 + 1);
+	planet->masse = atof(virgule4 + 1);
+	planet->radius = atof(virgule5 + 1);
+	planet->fullOrbitTime = atof(virgule6 + 1);
+	planet->gravity = atof(virgule7 + 1);
+	
+    return 1;
+}
+
+int lireFichier(char * nomFichier, struct donneePlanet * tableauARemplir, int longueur) {
+
+    FILE * file = fopen(nomFichier, "r");
+    if (file == NULL) return -1;
+
+    int ligne = 1;
+    char buffer[100];
+    while (fgets(buffer, 100, file) != NULL) {
+        if (ligne >= longueur) return ligne;
+        int ok = lireLigne(buffer, tableauARemplir + ligne);
+        if (ok) ligne = ligne + 1;
+    }
+    fclose(file);
+    return ligne;
+}
+
+
+int main(int argc, char **argv)
+{	
+	struct donneePlanet planete[1000];
+	int nbplanetes = lireFichier("test1.csv", planete, 1000);
+	afficher(planete);
+	return 0;
+}
